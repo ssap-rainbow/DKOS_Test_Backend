7bec4f4fb52988ffc30d59b8b8f44e77fdbef199
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-10-28 01:26:52 +0900

    Update init.sql

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -2,6 +2,8 @@ CREATE SCHEMA IF NOT EXISTS `krampoline` DEFAULT CHARACTER SET utf8mb4;
 
 GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
 GRANT ALL ON krampoline.* TO 'root'@'localhost';
+GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
+GRANT ALL ON krampoline.* TO 'root'@'%';
 FLUSH PRIVILEGES;
 
 USE `krampoline`;
