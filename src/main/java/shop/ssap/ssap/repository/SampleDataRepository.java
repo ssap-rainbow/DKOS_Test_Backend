9ded989a4ad117b5517fdf6a416d9a0ec062f59b
ssap-rainbow <ssap.rainbow@gmail.com>
2023-10-29 22:59:11 +0900

    feature: DB 정보 조회 test
    DB 정보 조회 test

---
+++ src/main/java/shop/ssap/ssap/repository/SampleDataRepository.java
@@ -0,0 +1,6 @@
+package shop.ssap.ssap.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SampleDataRepository extends JpaRepository<SampleData, Integer> {
+}
