4d4152217127a8e5aba7ddf733a2837d5d84d344
ssap-rainbow <ssap.rainbow@gmail.com>
2023-10-30 12:08:01 +0900

    feature: swagger 적용
    swagger 적용

--- src/main/java/shop/ssap/ssap/repository/SampleDataRepository.java
+++ src/main/java/shop/ssap/ssap/repository/SampleDataRepository.java
@@ -2,5 +2,8 @@ package shop.ssap.ssap.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SampleDataRepository extends JpaRepository<SampleData, Integer> {
+    Optional<SampleData> findByDetail(String detail);
 }
