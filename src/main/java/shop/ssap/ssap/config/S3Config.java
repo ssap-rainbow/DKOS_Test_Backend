f693b6f34b2f882e66f1fe017b23176cc95770ff
ssap-rainbow <ssap.rainbow@gmail.com>
2023-11-01 14:13:17 +0900

    feature: AWS S3 파일 업로드
    AWS S3 파일 업로드

---
+++ src/main/java/shop/ssap/ssap/config/S3Config.java
@@ -0,0 +1,32 @@
+package shop.ssap.ssap.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+    @Value("${AWS_S3_BUCKET}")
+    private String bucketName;
+
+    @Value("${AWS_REGION}")
+    private String region;
+
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String accessKeyId;
+
+    @Value("${AWS_SECRET_ACCESS_KEY}")
+    private String secretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretKey)))
+                .build();
+    }
+}
---
+++ src/main/java/shop/ssap/ssap/controller/FileUploadController.java
@@ -0,0 +1,41 @@
+package shop.ssap.ssap.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class FileUploadController {
+    @Autowired
+    private S3Client s3Client;
+
+    @Value("${AWS_S3_BUCKET}")
+    private String bucketName;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            s3Client.putObject(PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(file.getOriginalFilename())
+                            .build(),
+                    RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+            return ResponseEntity.ok("File uploaded successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("Failed to upload.");
+        }
+    }
+}
