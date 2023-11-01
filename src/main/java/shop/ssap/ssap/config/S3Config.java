489602a9a5ac811da2d79b235480e6f0b0ecf96e
ssap-rainbow <ssap.rainbow@gmail.com>
2023-11-02 00:08:10 +0900

    feature: 프록시 설정
    프록시 설정

--- src/main/java/shop/ssap/ssap/config/S3Config.java
+++ src/main/java/shop/ssap/ssap/config/S3Config.java
@@ -5,8 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.http.apache.ProxyConfiguration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
 
 @Configuration
 public class S3Config {
@@ -22,11 +27,32 @@ public class S3Config {
     @Value("${AWS_SECRET_ACCESS_KEY}")
     private String secretKey;
 
+    @Value("${PROXY_HOST}")
+    private String proxyHost;
+
+    @Value("${PROXY_PORT}")
+    private int proxyPort;
+
     @Bean
     public S3Client s3Client() {
+        SdkHttpClient httpClient = ApacheHttpClient.builder()
+                .proxyConfiguration(ProxyConfiguration.builder()
+                        .endpoint(URI.create("http://" + proxyHost + ":" + proxyPort))
+                        .build())
+                .build();
+
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretKey)))
+                .httpClient(httpClient)
                 .build();
     }
+
+//    @Bean
+//    public S3Client s3Client() {
+//        return S3Client.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretKey)))
+//                .build();
+//    }
 }
--- src/main/java/shop/ssap/ssap/controller/FileUploadController.java
+++ src/main/java/shop/ssap/ssap/controller/FileUploadController.java
@@ -4,15 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
