c04bf8cf58d7d29dbab63916b9621cc5d20666a6
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-01 15:49:10 +0900

    Update FileUploadController.java

--- src/main/java/shop/ssap/ssap/controller/FileUploadController.java
+++ src/main/java/shop/ssap/ssap/controller/FileUploadController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
