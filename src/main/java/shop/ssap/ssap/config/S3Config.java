ca0a2959353c2519cf6ca17dac89ff0863c13f8d
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-01 23:44:50 +0900

    Update S3Config.java

--- src/main/java/shop/ssap/ssap/config/S3Config.java
+++ src/main/java/shop/ssap/ssap/config/S3Config.java
@@ -5,28 +5,54 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.http.apache.ProxyConfiguration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+import java.net.URI;
+
 @Configuration
 public class S3Config {
-    @Value("${AWS_S3_BUCKET:defaultBucket}")
+    @Value("${AWS_S3_BUCKET}")
     private String bucketName;
 
-    @Value("${AWS_REGION:defaultBucket}")
+    @Value("${AWS_REGION}")
     private String region;
 
-    @Value("${AWS_ACCESS_KEY_ID:defaultBucket}")
+    @Value("${AWS_ACCESS_KEY_ID}")
     private String accessKeyId;
 
-    @Value("${AWS_SECRET_ACCESS_KEY:defaultBucket}")
+    @Value("${AWS_SECRET_ACCESS_KEY}")
     private String secretKey;
+
+    @Value("${proxy.host}")
+    private String proxyHost;
+
+    @Value("${proxy.port}")
+    private int proxyPort;
 
     @Bean
     public S3Client s3Client() {
+        SdkHttpClient httpClient = ApacheHttpClient.builder()
+                .proxyConfiguration(ProxyConfiguration.builder()
+                        .endpoint(URI.create("http://" + proxyHost + ":" + proxyPort))
+                        .build())
+                .build();
+
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretKey)))
+                .httpClient(httpClient)
                 .build();
     }
+
+//    @Bean
+//    public S3Client s3Client() {
+//        return S3Client.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretKey)))
+//                .build();
+//    }
 }
