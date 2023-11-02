e79df08400c4d8f3a8391a264ea286f67152ed41
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-02 18:10:44 +0900

    Update SwaggerConfig.java

--- src/main/java/shop/ssap/ssap/config/SwaggerConfig.java
+++ src/main/java/shop/ssap/ssap/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
         info = @Info(title = "SSAP 심부름 플랫폼 API 명세서",
                 description = "SSAP 심부름 플랫폼 API 명세서",
-                version = "v3"))
+                version = "v1"))
 @Configuration
 public class SwaggerConfig {
     @Bean
@@ -19,6 +19,6 @@ public class SwaggerConfig {
                 .info(new io.swagger.v3.oas.models.info.Info()
                         .title("SSAP 심부름 플랫폼 API 명세서")
                         .description("SSAP 심부름 플랫폼 API 명세서")
-                        .version("v3"));
+                        .version("v1"));
     }          
 }
