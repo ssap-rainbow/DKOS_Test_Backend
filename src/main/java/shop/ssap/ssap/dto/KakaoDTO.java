c4672766dae7d4167c6b8b5a3ad9f59269284b4d
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-04 23:18:54 +0900

    Create KakaoDTO.java

---
+++ src/main/java/shop/ssap/ssap/dto/KakaoDTO.java
@@ -0,0 +1,12 @@
+package ssap.ssap.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class KakaoDTO {
+    private long id;
+    private String email;
+    private String nickname;
+}
