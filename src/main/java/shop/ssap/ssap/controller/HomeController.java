9a2624a5e783106eaa9b74090c8752b16a101684
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-05 00:13:56 +0900

    Update HomeController.java

--- src/main/java/shop/ssap/ssap/controller/HomeController.java
+++ src/main/java/shop/ssap/ssap/controller/HomeController.java
@@ -3,7 +3,7 @@
 // import io.swagger.v3.oas.annotations.tags.Tag;
 // import org.springframework.http.ResponseEntity;
 // import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;
 // import shop.ssap.ssap.repository.SampleData;
 // import shop.ssap.ssap.repository.SampleDataRepository;
 
