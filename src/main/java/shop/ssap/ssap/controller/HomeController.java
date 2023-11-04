a49310209a0aa1be4508fc177116609b10833451
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-04 23:16:46 +0900

    Update HomeController.java

--- src/main/java/shop/ssap/ssap/controller/HomeController.java
+++ src/main/java/shop/ssap/ssap/controller/HomeController.java
@@ -1,49 +1,75 @@
+// package shop.ssap.ssap.controller;
+
+// import io.swagger.v3.oas.annotations.tags.Tag;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.*;
+// import shop.ssap.ssap.repository.SampleData;
+// import shop.ssap.ssap.repository.SampleDataRepository;
+
+// import java.util.Optional;
+
+// @CrossOrigin(origins = "*") 
+// @Controller
+// @Tag(name = "Home 컨트롤러", description = "HOME API입니다.")
+// public class HomeController {
+
+//     private final SampleDataRepository sampleDataRepository;
+
+//     public HomeController(SampleDataRepository sampleDataRepository) {
+//         this.sampleDataRepository = sampleDataRepository;
+//     }
+
+//     @RequestMapping(value = "/api/hello", method = RequestMethod.GET)
+//     @ResponseBody
+//     public String hello() {
+//         return "hello";
+//     }
+
+//     @RequestMapping(value = "/api/greeting", method = RequestMethod.POST)
+//     @ResponseBody
+//     public String greeting(@RequestBody String name) {
+//         return "Hello, " + name + "!";
+//     }
+
+
+
+//     @RequestMapping(value="/api/home", method= RequestMethod.GET)
+//     @ResponseBody
+//     public ResponseEntity<String> home() {
+//         // "Hello DKOS!" 값을 가진 데이터만 검색
+//         Optional<SampleData> helloDkosData = sampleDataRepository.findByDetail("Hello DKOS!");
+        
+//         if (helloDkosData.isPresent()) {
+//             return ResponseEntity.ok(helloDkosData.get().getDetail());
+//         } else {
+//             return ResponseEntity.notFound().build();
+//         }
+//     }
+// }
+
+
 package shop.ssap.ssap.controller;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import shop.ssap.ssap.repository.SampleData;
-import shop.ssap.ssap.repository.SampleDataRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import shop.ssap.ssap.service.KakaoService;
 
-import java.util.Optional;
-
-@CrossOrigin(origins = "*") 
+@RequiredArgsConstructor
 @Controller
-@Tag(name = "Home 컨트롤러", description = "HOME API입니다.")
 public class HomeController {
 
-    private final SampleDataRepository sampleDataRepository;
+    private final KakaoService kakaoService;
 
-    public HomeController(SampleDataRepository sampleDataRepository) {
-        this.sampleDataRepository = sampleDataRepository;
-    }
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
 
-    @RequestMapping(value = "/api/hello", method = RequestMethod.GET)
-    @ResponseBody
-    public String hello() {
-        return "hello";
-    }
-
-    @RequestMapping(value = "/api/greeting", method = RequestMethod.POST)
-    @ResponseBody
-    public String greeting(@RequestBody String name) {
-        return "Hello, " + name + "!";
+        return "index";
     }
 
 
-
-    @RequestMapping(value="/api/home", method= RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity<String> home() {
-        // "Hello DKOS!" 값을 가진 데이터만 검색
-        Optional<SampleData> helloDkosData = sampleDataRepository.findByDetail("Hello DKOS!");
-        
-        if (helloDkosData.isPresent()) {
-            return ResponseEntity.ok(helloDkosData.get().getDetail());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
 }
