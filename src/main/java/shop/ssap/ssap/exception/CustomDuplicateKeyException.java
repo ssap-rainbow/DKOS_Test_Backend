e23b913680aa780fa2ba05b191c5beeface350b9
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-09 19:33:52 +0900

    Create CustomDuplicateKeyException.java

---
+++ src/main/java/shop/ssap/ssap/exception/CustomDuplicateKeyException.java
@@ -0,0 +1,9 @@
+package shop.ssap.ssap.exception;
+
+public class CustomDuplicateKeyException extends RuntimeException {
+
+    public CustomDuplicateKeyException(String message) {
+        super(message);
+    }
+}
+
