942c874c5d2f2d9f92feaf9631a5c6b02d4ec639
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-09 19:38:12 +0900

    Update KakaoOAuthService.java

--- src/main/java/shop/ssap/ssap/service/KakaoOAuthService.java
+++ src/main/java/shop/ssap/ssap/service/KakaoOAuthService.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
