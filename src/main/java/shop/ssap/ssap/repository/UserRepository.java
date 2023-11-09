76c9e0708dad4fb89abcdb360df10263c5be9f2c
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-09 17:45:44 +0900

    Create UserRepository.java

---
+++ src/main/java/shop/ssap/ssap/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package shop.ssap.ssap.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import shop.ssap.ssap.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderId(String providerId);
+
+}
