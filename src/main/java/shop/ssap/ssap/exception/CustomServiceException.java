9afb7c5487d2c08560e19bf973805b250cdf979d
ssap-rainbow <148201499+ssap-rainbow@users.noreply.github.com>
2023-11-13 17:08:08 +0900

    Create CustomServiceException.java

---
+++ src/main/java/shop/ssap/ssap/exception/CustomServiceException.java
@@ -0,0 +1,7 @@
+package shop.ssap.ssap.exception;
+
+public class CustomServiceException extends RuntimeException {
+    public CustomServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
